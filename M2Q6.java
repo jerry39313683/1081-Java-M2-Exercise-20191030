5cb81e30d2be2fcaa585d1d65e545a1fbb75241b
SKRB <56062164+jerry39313683@users.noreply.github.com>
2019-11-19 22:56:28 +0800

    Update M2Q6.java

--- M2Q6.java
+++ M2Q6.java
@@ -3,7 +3,7 @@ public class M206{
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
     int n =scn.nextInt();
-    int m =n/2;
+    int m = 0;
     boolean flag = true;
     for( int i=2 ; i<=m ; i++){
         if(n%i==0){
