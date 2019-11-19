2b17e06a8cad995134fac7613d7861fedd65b8b3
SKRB <56062164+jerry39313683@users.noreply.github.com>
2019-11-19 22:56:40 +0800

    Update M2Q6.java

--- M2Q6.java
+++ M2Q6.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
-public class M206{
+public class M2Q6{
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-    int n =scn.nextInt();
-    int m = 0;
-    boolean flag = true;
-    for( int i=2 ; i<=m ; i++){
+        int n = scn.nextInt();
+        int m = 0;
+        boolean flag = true;
+        for(int i=2 ; i<=n ;i++){
         if(n%i==0){
-            flag = false;
-            break;
+           flag = false;
+           break;
+        }           
+        }
+        if(flag){
+            System.out.println("No");
+        }
+        else{
+            System.out.println("Yes");
         }
     }
-    if(flag){
-        System.out.println("NO");
     }
-    else{
-        System.out.println("YES");
-    }
-}
-}
